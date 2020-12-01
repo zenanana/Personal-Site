ffb0dd91a8897d80a8987352a41089dd469fe533
kabusechasencha <59784343+kabusechasencha@users.noreply.github.com>
2020-12-01 23:01:55 +0800

    Updates

---
+++ precache-manifest.8b97bb6717bf46af3baf504928a590f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bdf609191b041ac8a5be79851182682",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec671d55007bdb8693e7",
+    "url": "/static/css/main.8bf40d0b.chunk.css"
+  },
+  {
+    "revision": "8780c8c31d352bdff7e1",
+    "url": "/static/js/2.20f6db7c.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.20f6db7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec671d55007bdb8693e7",
+    "url": "/static/js/main.556dcc28.chunk.js"
+  },
+  {
+    "revision": "b63a4b8ca5166987a4a7",
+    "url": "/static/js/runtime-main.2ca0876f.js"
+  },
+  {
+    "revision": "0d87e6913e64a11189278400b4e0ed46",
+    "url": "/static/media/CircularStd-Light.0d87e691.otf"
+  },
+  {
+    "revision": "70cf25f4aa97d74d91ca51569c30178d",
+    "url": "/static/media/resume.70cf25f4.pdf"
+  }
+]);
