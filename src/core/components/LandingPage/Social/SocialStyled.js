c68f84ea69b5bb5c1debc6751db2bc78b4e0c2fb
kabusechasencha <59784343+kabusechasencha@users.noreply.github.com>
2021-05-15 13:29:48 +0800

    Updates

--- src/core/components/LandingPage/Social/SocialStyled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Image = styled.img`
-  max-width: 50px;
-  max-height: 50px;
-  position: relative;
-  padding-right: 5px;
-  padding-left: 5px;
-`;
